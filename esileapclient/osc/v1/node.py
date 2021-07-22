62f68223b44b26bdb6d349b80f3c682609d7c96e
Elliot Brock <beta.redex@gmail.com>
2021-07-22 12:55:15 -0400

    Add resource list functionality

---
+++ esileapclient/osc/v1/node.py
@@ -0,0 +1,60 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import logging
+
+from osc_lib.command import command
+from osc_lib import utils as oscutils
+
+from esileapclient.v1.node import Node as NODE_RESOURCE
+
+LOG = logging.getLogger(__name__)
+
+
+class ListNode(command.Lister):
+    """List nodes."""
+
+    log = logging.getLogger(__name__ + ".ListNode")
+
+    def get_parser(self, prog_name):
+        parser = super(ListNode, self).get_parser(prog_name)
+
+        # Detailed information is the same as not
+        # detailed right now, will be expanded later
+        parser.add_argument(
+            '--long',
+            default=False,
+            help="Show detailed information about the nodes.",
+            action='store_true')
+
+        return parser
+
+    def take_action(self, parsed_args):
+
+        client = self.app.client_manager.lease
+
+        # No filters yet
+        filters = {
+        }
+
+        data = client.node.list(filters)
+
+        # These two are the same right now, they just give the name
+        if parsed_args.long:
+            columns = NODE_RESOURCE.detailed_fields.keys()
+            labels = NODE_RESOURCE.detailed_fields.values()
+        else:
+            columns = NODE_RESOURCE.fields.keys()
+            labels = NODE_RESOURCE.fields.values()
+
+        return (labels,
+                (oscutils.get_item_properties(s, columns) for s in data))
---
+++ esileapclient/v1/node.py
@@ -0,0 +1,54 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+import logging
+
+from esileapclient.common import base
+
+LOG = logging.getLogger(__name__)
+
+
+class Node(base.Resource):
+
+    detailed_fields = {
+        'name': "Name"
+    }
+
+    fields = {
+        'name': "Name"
+    }
+
+    _creation_attributes = ['name']
+
+    def __repr__(self):
+        return "<Node %s>" % self._info
+
+
+class NodeManager(base.Manager):
+    resource_class = Node
+    _resource_name = 'nodes'
+
+    def list(self, filters, os_esileap_api_version=None):
+        """Retrieve a list of nodes.
+        :returns: A list of nodes.
+        """
+
+        resource_id = ""
+
+        url_variables = NodeManager._url_variables(filters)
+        url = self._path(resource_id) + url_variables
+
+        nodes = self._list(url, os_esileap_api_version=os_esileap_api_version)
+
+        if type(nodes) is list:
+            return nodes
