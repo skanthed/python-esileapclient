0701a6885b965073c404f8f91c645dc03c5cb791
stack <samuel_zuk@student.uml.edu>
2021-08-04 21:07:58 +0000

    Implemented functional policy tests.

---
+++ esileapclient/tests/functional/v1/test_policy.py
@@ -0,0 +1,133 @@
+from tempest.lib.exceptions import CommandFailed
+from tempest.lib import decorators
+from tempest.lib.common.utils import data_utils
+
+import esileapclient.tests.functional.utils.esi_interfaces as esi
+from esileapclient.tests.functional.base import ESIBaseTestClass
+from esileapclient.tests.functional.utils.dummy_node import DummyNode
+
+
+class PolicyTests(ESIBaseTestClass):
+    @classmethod
+    def setUpClass(cls):
+        super(PolicyTests, cls).setUpClass()
+        cls._init_dummy_project(cls, 'authorized', ['owner'])
+        cls._init_dummy_project(cls, 'unauthorized', ['owner', 'lessee'])
+
+    def setUp(self):
+        super(PolicyTests, self).setUp()
+        self.clients = PolicyTests.clients
+        self.users = PolicyTests.users
+        self.projects = PolicyTests.projects
+        self.dummy_node = DummyNode(PolicyTests.config['dummy_node_dir'],
+                                    self.projects['authorized']['id'])
+
+    @decorators.attr(type=['negative'])
+    def test_create_lessee(self):
+        """ Tests that a lessee cannot create an offer or a lease.
+            Test steps:
+            1) Attempt to create an offer as a lessee.
+            2) Check that the command failed. (returned non-zero exit code)
+            3) Check that the proper error message was sent to stderr.
+            4) Attempt to create a lease as a lessee (using lease create).
+            5) Check that the command failed. (returned non-zero exit code)
+            6) Check that the proper error message was sent to stderr. """
+        e = self.assertRaises(CommandFailed,
+                              esi.offer_create,
+                              self.clients['unauthorized-lessee'],
+                              self.dummy_node.uuid,
+                              resource_type='dummy_node')
+        self.assertIn('Access was denied to esi_leap:offer:create.',
+                      e.stderr.decode())
+
+        e = self.assertRaises(CommandFailed,
+                              esi.lease_create,
+                              self.clients['unauthorized-lessee'],
+                              self.dummy_node.uuid,
+                              self.projects['unauthorized']['name'],
+                              resource_type='dummy_node')
+        self.assertIn('Access was denied to esi_leap:lease:create.',
+                      e.stderr.decode())
+
+    @decorators.attr(type=['negative'])
+    def test_offer_delete_lessee(self):
+        """ Tests that a lessee cannot delete an offer.
+            Test steps:
+            1) (authorized-owner) Create an offer for an owned node for the
+                unauthorized project.
+            2) Check that lease details were returned.
+            3) (unauthorized-lessee) Attempt to delete the new offer.
+            4) Check that the command failed. (returned non-zero exit code)
+            5) Check that the proper error message was sent to stderr.
+            6) (authorized-owner) (cleanup) Delete the offer from step 1. """
+        offer = esi.offer_create(self.clients['authorized-owner'],
+                                 self.dummy_node.uuid,
+                                 lessee=self.projects['unauthorized']['name'],
+                                 resource_type='dummy_node')
+        self.assertNotEqual(offer, {})
+        self.addCleanup(esi.offer_delete,
+                        self.clients['authorized-owner'],
+                        offer['uuid'])
+
+        e = self.assertRaises(CommandFailed,
+                              esi.offer_delete,
+                              self.clients['unauthorized-lessee'],
+                              offer['uuid'])
+        self.assertIn('Access was denied to esi_leap:offer:delete.',
+                      e.stderr.decode())
+
+    @decorators.attr(type=['negative'])
+    def test_create_unauthorized(self):
+        """ Tests that an owner can't create an offer or a lease for a node
+                that they do not own.
+            1) (unauthorized-owner) Attempt to create an offer for the dummy 
+                node owned by the authorized project.
+            2) Check that the command failed. (returned non-zero exit code)
+            3) Check that the proper error message was sent to stderr.
+            1) (unauthorized-owner) Attempt to create a lease on the dummy 
+                node owned by the authorized project.
+            5) Check that the command failed. (returned non-zero exit code)
+            6) Check that the proper error message was sent to stderr. """
+        e = self.assertRaises(CommandFailed,
+                              esi.offer_create,
+                              self.clients['unauthorized-owner'],
+                              self.dummy_node.uuid,
+                              resource_type='dummy_node')
+        self.assertIn('Access was denied to dummy_node %s.' %
+                      self.dummy_node.uuid, e.stderr.decode())
+
+        e = self.assertRaises(CommandFailed,
+                              esi.lease_create,
+                              self.clients['unauthorized-owner'],
+                              self.dummy_node.uuid,
+                              self.projects['unauthorized']['name'],
+                              resource_type='dummy_node')
+        self.assertIn('Access was denied to dummy_node %s.' %
+                      self.dummy_node.uuid, e.stderr.decode())
+
+    @decorators.attr(type=['negative'])
+    def test_offer_claim_unauthorized(self):
+        """ Tests that a lessee cannot delete an offer.
+            Test steps:
+            1) (authorized-owner) Create an offer for an owned node without
+                specifying a lessee.
+            2) Check that lease details were returned.
+            3) (unauthorized-lessee) Attempt to claim the offer from step 1.
+            4) Check that the command failed. (returned non-zero exit code)
+            5) Check that the proper error message was sent to stderr.
+            6) (authorized-owner) (cleanup) Delete the offer from step 1. """
+        offer = esi.offer_create(self.clients['authorized-owner'],
+                                 self.dummy_node.uuid,
+                                 lessee=self.projects['authorized']['name'],
+                                 resource_type='dummy_node')
+        self.assertNotEqual(offer, {})
+        self.addCleanup(esi.offer_delete,
+                        self.clients['authorized-owner'],
+                        offer['uuid'])
+
+        e = self.assertRaises(CommandFailed,
+                              esi.offer_claim,
+                              self.clients['unauthorized-lessee'],
+                              offer['uuid'])
+        self.assertIn('Access was denied to offer %s.' % offer['uuid'],
+                      e.stderr.decode())
